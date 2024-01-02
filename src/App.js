1184bb08a468171f9c4a288006d6f887bf5ce296
shuja-devops <shuja.akbar@blueeast.com>
2024-01-03 04:53:53 +0500

    fiverr demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import fiverr from './fiverr.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={fiverr} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello fiverr, You want's to live your application.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact Me
         </a>
       </header>
     </div>
